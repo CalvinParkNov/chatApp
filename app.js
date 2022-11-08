68b450c5c5dbb21085a94d89c95bd87cc2819010
CalvinParkNov <dev.calvinpark@gmail.com>
2022-11-09 01:32:34 +0900

    feat: prettier added

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
-const connectDB = require("./config/connect");
+const connectDB = require('./config/connect');
 const router = express.Router();
-const helmet = require("helmet");
-const morgan = require("morgan");
-const multer = require("multer");
-const path = require("path");
+const helmet = require('helmet');
+const morgan = require('morgan');
+const multer = require('multer');
+const path = require('path');
 
 //middleware
 app.use(express.json());
 app.use(helmet());
-app.use(morgan("common"));
+app.use(morgan('common'));
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
