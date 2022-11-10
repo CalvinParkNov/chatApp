7a7b9ecccffb3f4fbe74025e1e43a01e29d06096
CalvinParkNov <dev.calvinpark@gmail.com>
2022-11-11 01:24:56 +0900

    feat: post added

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,10 +1,12 @@
 import Share from "../share/Share";
 import "./feed.css";
+import Post from "../post/Post";
 
 export default function feed() {
   return (
     <div className="feedWrapper">
       <Share />
+      <Post />
     </div>
   );
 }
