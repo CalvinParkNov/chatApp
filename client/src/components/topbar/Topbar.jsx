e637f1270734572b595db625f12507f7a9d83fa8
CalvinParkNov <dev.calvinpark@gmail.com>
2022-11-10 20:49:40 +0900

    feat: sidebar icons

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -9,7 +9,7 @@ export default function Topbar() {
       </div>
       <div className="topbarCenter">
         <div className="searchbar">
-          <Search />
+          <Search className="searchIcon" />
           <input className="searchInput" placeholder="Search" />
         </div>
       </div>
@@ -21,15 +21,15 @@ export default function Topbar() {
         <div className="topbarIcons">
           <div className="topbarIconItem">
             <Person />
-            <span className="topbarBadge">1</span>
+            <span className="topbarIconBadge">1</span>
           </div>
           <div className="topbarIconItem">
             <Chat />
-            <span className="topbarBadge">2</span>
+            <span className="topbarIconBadge">2</span>
           </div>
           <div className="topbarIconItem">
             <Notifications />
-            <span className="topbarBadge">10</span>
+            <span className="topbarIconBadge">10</span>
           </div>
         </div>
         <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
