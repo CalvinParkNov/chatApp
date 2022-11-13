363f481831fff00fe69ddcf4a371796fd2fa5351
CalvinParkNov <dev.calvinpark@gmail.com>
2022-11-13 22:49:07 +0900

    feat: post data via dummy data

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,12 +1,15 @@
 import Share from "../share/Share";
 import "./feed.css";
 import Post from "../post/Post";
+import { Posts } from "../../dummyData";
 
 export default function feed() {
   return (
     <div className="feedWrapper">
       <Share />
-      <Post />
+      {Posts.map((e) => (
+        <Post key={e.id} post={e} />
+      ))}
     </div>
   );
 }
---
+++ client/src/dummyData.js
@@ -0,0 +1,60 @@
+export const Users = [
+  {
+    id: 1,
+    profilePicture: "assets/person/1.jpeg",
+    username: "Calvin Park",
+  },
+  {
+    id: 2,
+    profilePicture: "assets/person/2.jpeg",
+    username: "Won Park",
+  },
+  {
+    id: 3,
+    profilePicture: "assets/person/3.jpeg",
+    username: "John Park",
+  },
+  {
+    id: 4,
+    profilePicture: "assets/person/3.jpeg",
+    username: "Won Kyun Park",
+  },
+];
+
+export const Posts = [
+  {
+    id: 1,
+    desc: "Here we go",
+    photo: "assets/post/1.jpeg",
+    date: "5 mins ago",
+    userId: 1,
+    like: 32,
+    comment: 9,
+  },
+  {
+    id: 2,
+    desc: "Here we go",
+    photo: "assets/post/2.jpeg",
+    date: "5 mins ago",
+    userId: 2,
+    like: 32,
+    comment: 9,
+  },
+  {
+    id: 3,
+    desc: "What ever",
+    photo: "assets/post/3.jpeg",
+    date: "10 mins ago",
+    userId: 3,
+    like: 32,
+    comment: 9,
+  },
+  {
+    id: 4,
+    photo: "assets/post/4.jpeg",
+    date: "5 mins ago",
+    userId: 4,
+    like: 32,
+    comment: 910000,
+  },
+];
