f679aba11acf4d5b58543f9a8d58f82d0266405e
CalvinParkNov <dev.calvinpark@gmail.com>
2022-12-06 21:18:32 +0900

    change file name

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -3,5 +3,8 @@ import Express from "express";
 import { getUser } from "../controllers/user.controller.js";
 const router = Express.Router();
 
+router.get("/", (req, res) => {
+  res.send("asdfasf");
+});
 router.get("/find/:userId", getUser);
 export default router;
