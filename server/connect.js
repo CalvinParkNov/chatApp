76218e9aeefef024bb24822ba4f31537cf161ef0
CalvinParkNov <dev.calvinpark@gmail.com>
2022-12-24 22:52:01 +0900

    mysql host connection

--- server/connect.js
+++ server/connect.js
@@ -1,6 +1,9 @@
 import mysql from "mysql";
+import * as dotenv from "dotenv";
+dotenv.config();
+
 export const db = mysql.createConnection({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
   password: process.env.DB_PASSWORD,
   database: process.env.DB,
