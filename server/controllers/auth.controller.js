dea6fc4b3d1dd3a8d47c3121185ff09bad6e40a7
CalvinParkNov <dev.calvinpark@gmail.com>
2022-12-25 18:50:47 +0900

    feat: logout

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -73,4 +73,12 @@ export const login = (req, res) => {
   });
 };
 
-export const logout = (req, res) => {};
+export const logout = (req, res) => {
+  res
+    .clearCookie("accessToken", {
+      secure: true,
+      sameSite: "none",
+    })
+    .status(200)
+    .json("User logged out");
+};
